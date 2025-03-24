39fa40a9b07ce86eda758155310b4ce05ba38f4d
Etrav05 <Etravis090705@gmail.com>
2025-03-23 22:40:04 -0400

    - Added parameter to sort, print data, and print min-max for any inputted feature value (0->3)

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ def testFunctionCalls():
     print("Feature Data: ")  # Flower measurements:
     print(feature_data[:5])                 # (sepal length, sepal width, petal length, petal width)
 
-    print("Target Label: ") # Species labels:
+    print("Target Label: ")  # Species labels:
     print(label[:5])                 # (0 = setosa, 1 = versicolor, 2 = virginica)
 
     print("Feature name: ", feature_names)
 
     print("Target name: ", target_names)
-def printSpeciesData(feature, feature_data, label, target_names): # pass these parameters so we can work with copys
+def printSpeciesData(feature, feature_data, label, target_names):  # pass these parameters so we can work with copys
     print("#. Species, feature data")
 
     n = len(feature_data)
     for x in range(n):
         if label[x] == 0:
-            print(x + 1, "-", feature_data[x][feature], "cm",target_names[0]) # print based on species name
+            print(x + 1, "-", feature_data[x][feature], "cm", target_names[0])  # print based on species name
 
         elif label[x] == 1:
             print(x + 1, "-", feature_data[x][feature], "cm", target_names[1])
@@ -47,8 +47,8 @@ def selectionSort(feature, feature_data, label):
         min_idx = x
 
         for y in range(x + 1, n):
-            if (feature_data[y][feature] < feature_data[min_idx][feature]): # sort by first feature (sepal length)
-                min_idx = y # change min to this new index
+            if (feature_data[y][feature] < feature_data[min_idx][feature]):  # sort by first feature (sepal length)
+                min_idx = y  # change min to this new index
 
         swap(feature, feature_data, label, x, min_idx)
 
@@ -56,14 +56,14 @@ def printMinMax_SLen(feature, feature_data):
     n = len(feature_data)
     print("min", feature_data[1][feature], "---->", feature_data[n - 1][feature], "max")
 
-feature_data_copy = feature_data.copy() # create personal copy of data sets
+feature_data_copy = feature_data.copy()  # create personal copy of data sets
 label_copy = label.copy()
 
 # main
-feature = 3 # 0 = sepal length, 1 = sepal width, 2 = petal length, 3 = petal length
+featureValue = 3  # 0 = sepal length, 1 = sepal width, 2 = petal length, 3 = petal length
 
-selectionSort(feature, feature_data_copy, label_copy)
+selectionSort(featureValue, feature_data_copy, label_copy)
 
-printSpeciesData(feature, feature_data_copy, label_copy, target_names)
+printSpeciesData(featureValue, feature_data_copy, label_copy, target_names)
 
-printMinMax_SLen(feature, feature_data_copy)
+printMinMax_SLen(featureValue, feature_data_copy)
